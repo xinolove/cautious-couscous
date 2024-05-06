fc76d498b8941379405024f12ebf5f162ffd1fb3
2733220798@qq.com <299732458@qq.com>
2024-05-07 07:54:32 +0800

    TEST

--- LOLVSlike/main.cpp
+++ LOLVSlike/main.cpp
@@ -1,18 +1,33 @@
 #include<graphics.h>
 #include<string>
+#include<vector>
 
-int idx_current_anim = 0;
+#pragma comment(lib, "MSIMG32.LIB")
 
-const int PLAYER_SPEED = 6;
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
 const int PLAYER_ANIM_NUM = 6;
 
 IMAGE img_player_left[PLAYER_ANIM_NUM];
 IMAGE img_player_right[PLAYER_ANIM_NUM];
 
-POINT player_pos = { 500,500 };
+void LoadAnimation()
+{
+	loadimage(&img_player_left[1], _T("img/player_left_1.png"));
+	loadimage(&img_player_left[2], _T("img/player_left_2.png"));
+	loadimage(&img_player_left[3], _T("img/player_left_3.png"));
+	loadimage(&img_player_left[4], _T("img/player_left_4.png"));
+	loadimage(&img_player_left[5], _T("img/player_left_5.png"));
+	loadimage(&img_player_left[6], _T("img/player_left_6.png"));
 
-#pragma comment(lib, "MSIMG32.LIB")
+	loadimage(&img_player_right[1], _T("img/player_right_1.png"));
+	loadimage(&img_player_right[2], _T("img/player_right_2.png"));
+	loadimage(&img_player_right[3], _T("img/player_right_3.png"));
+	loadimage(&img_player_right[4], _T("img/player_right_4.png"));
+	loadimage(&img_player_right[5], _T("img/player_right_5.png"));
+	loadimage(&img_player_right[6], _T("img/player_right_6.png"));
+}
 
 inline void putimage_alpha(int x, int y, IMAGE* img)
 {
@@ -22,19 +37,296 @@ inline void putimage_alpha(int x, int y, IMAGE* img)
 		GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER,0,255,AC_SRC_ALPHA });
 }
 
-void LoadAnimation()
+class Animation
 {
-	for (size_t i = 0; i < PLAYER_ANIM_NUM; i++)
+public:
+	Animation(LPCSTR path, int num, int interval)
 	{
-		std::wstring path = L"img/player_left_" + std::to_wstring(i) + L".png";
-		loadimage(&img_player_left[i], path.c_str());
+		interval_ms = interval;
+
+		TCHAR path_file[256];
+		for (size_t i = 0; i < num; i++)
+		{
+			_stprintf_s(path_file, path, i);
+
+			IMAGE* frame = new IMAGE();
+			loadimage(frame, path_file);
+			frame_list.push_back(frame);
+		}
 	}
-	for (size_t i = 0; i < PLAYER_ANIM_NUM; i++)
+
+
+	void Play(int x, int y, int delta)
 	{
-		std::wstring path = L"img/player_right_" + std::to_wstring(i) + L".png";
-		loadimage(&img_player_right[i], path.c_str());
+		timer += delta;
+		if (timer >= interval_ms)
+		{
+			idx_frame = (idx_frame + 1) % frame_list.size();
+			timer = 0;
+		}
+
+		putimage_alpha(x, y, frame_list[idx_frame]);
 	}
+	~Animation()
+	{
+		for (size_t i = 0; i < frame_list.size(); i++)
+		{
+			delete frame_list[i];
+		}
+	}
+
+private:
+	int timer = 0;
+	int idx_frame = 0;
+	int interval_ms = 0;
+	std::vector<IMAGE*>frame_list;
+};
+
+
+class Player
+{
+public:
+	Player()
+	{
+		loadimage(&img_shadow, _T("img/shadow_player.png"));
+		anim_left=new Animation (_T("img/player_left_%d.png"), 6, 45);
+		anim_right=new Animation (_T("img/player_right_%d.png"), 6, 45);
+	}
+	
+
+	POINT GetPosition()const{
+		return position;
+	}
+
+	void ProcessEvent(const ExMessage& msg)
+	{
+		switch (msg.message)
+		{
+		case WM_KEYDOWN:
+			switch (msg.vkcode)
+			{
+			case 87:
+				is_move_up = true;
+				break;
+			case 83:
+				is_move_down = true;
+				break;
+			case 65:
+				is_move_left = true;
+				break;
+			case 68:
+				is_move_right = true;
+				break;
+			}
+			break;
+		
+		case WM_KEYUP:
+			switch (msg.vkcode)
+			{
+			case 87:
+				is_move_up = false;
+				break;
+			case 83:
+				is_move_down = false;
+				break;
+			case 65:
+				is_move_left = false;
+				break;
+			case 68:
+				is_move_right = false;
+				break;
+			}
+			break;
+		}
+	}
+
+	void Move()
+	{
+		int dir_x = is_move_right - is_move_left;
+		int dir_y = is_move_down - is_move_up;
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;
+			double normalized_y = dir_y / len_dir;
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
+		}
+
+		if (position.x < 0)position.x = 0;
+		if (position.y < 0)position.y = 0;
+		if (position.x + FRAME_WIDTH > WINDOW_WIDTH)position.x = WINDOW_WIDTH - FRAME_WIDTH;
+		if (position.y + FRAME_HEIGHT > WINDOW_HEIGHT)position.y = WINDOW_HEIGHT - FRAME_HEIGHT;
+
+	}
+
+	void Draw(int delta)
+	{
+		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 8;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+		static bool facing_left = false;
+		int dir_x = is_move_right - is_move_left;
+		if (dir_x < 0)
+			facing_left = true;
+		else if (dir_x > 0)
+			facing_left = false;
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+	}
+	~Player()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+private:
+	const int SPEED = 3;
+	const int FRAME_WIDTH = 80;
+	const int FRAME_HEIGHT = 80;
+	const int SHADOW_WIDTH = 32;
+
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right;
+	POINT position = { 500,500 };
+	bool is_move_up = false;
+	bool is_move_down = false;
+	bool is_move_left = false;
+	bool is_move_right = false;
+};
+
+class Bullet
+{
+public:
+	POINT position = { 0,0 };
+	
+	Bullet() = default;
+	~Bullet() = default;
+
+	void Draw()const
+	{
+		setlinecolor(RGB(0, 60, 20));
+		setfillcolor(RGB(20, 100, 45));
+		fillcircle(position.x, position.y, RADIUS);
+	}
+private:
+	const int RADIUS = 10;
+};
+
+class Enemy
+{
+public:
+	Enemy()
+	{
+		loadimage(&img_shadow, _T("img/shadow_enemy.png"));
+		anim_left = new Animation(_T("img/enemy_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("img/enemy_right_%d.png"), 6, 45);
+		
+		enum class SpawnEdge
+		{
+			Up=0,
+			Down,
+			Left,
+			Right
+		};
+
+		SpawnEdge edge = (SpawnEdge)(rand() % 4);
+		switch (edge)
+		{
+		case SpawnEdge::Up:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = -FRAME_HEIGHT;
+			break;
+		case SpawnEdge::Down:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = -WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Left:
+			position.x = -FRAME_WIDTH;
+			position.y =rand() % WINDOW_HEIGHT ;
+			break;
+		case SpawnEdge::Right:
+			position.x = -WINDOW_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		default:
+			break;
+		}
+	}
+	bool CheckBulletCollision(const Bullet& bullet)
+	{
+		return false;
+	}
+
+	bool CheckPlayerCollision(const Player& player)
+	{
+		return false;
+	}
+
+	void Move(const Player& player)
+	{
+		const POINT& player_position = player.GetPosition();
+		int dir_x = player_position.x - position.x;
+		int dir_y = player_position.y - position.y;
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;
+			double normalized_y = dir_y / len_dir;
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
+		}
+		if (dir_x < 0)
+			facing_left = true;
+		else if (dir_x > 0)
+			facing_left = false;
+	}
+
+	void Draw(int delta)
+	{
+		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 35;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+
+	}
+
+	~Enemy()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+private:
+	const int SPEED =2 ;
+	const int FRAME_WIDTH = 80;
+	const int FRAME_HEIGHT = 80;
+	const int SHADOW_WIDTH = 48;
+
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right;
+	POINT position = { 0,0 };
+	bool facing_left = false;
+};
+
+
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
+{
+	const int INTERVAL = 100;
+	static int counter = 0;
+	if ((++counter) % INTERVAL == 0)
+		enemy_list.push_back(new Enemy());
 }
+
 
 int main()
 {
@@ -42,81 +334,34 @@ int main()
 
 	bool running = true;
 
+	Player player;
 	ExMessage msg;
 	IMAGE img_background;
+	std::vector<Enemy*> enemy_list;
 
-	bool is_move_up = false;
-	bool is_move_down = false;
-	bool is_move_left = false;
-	bool is_move_right = false;
-
-	LoadAnimation();
 	loadimage(&img_background, _T("img/background.png"));
 
 	BeginBatchDraw();
 
 	while (running)
 	{
-		DWORD start_time = GetTickCount();
-
-
+		DWORD start_time = GetTickCount(); 
 		while (peekmessage(&msg))
 		{
-			if (msg.message == WM_KEYDOWN)
-			{
-				switch (msg.vkcode)
-				{
-				case 87:
-					is_move_up = true;
-					break;
-				case 83:
-					is_move_down = true;
-					break; 
-				case 65:
-					is_move_left = true;
-					break;
-				case 68:
-					is_move_right = true;
-					break;
-				}
-			}
-			else if (msg.message == WM_KEYUP)
-			{
-				switch (msg.vkcode)
-				{
-				case 87:
-					is_move_up = false;
-					break;
-				case 83:
-					is_move_down = false;
-					break;
-				case 65:
-					is_move_left = false;
-					break;
-				case 68:
-					is_move_right = false;
-					break;
-				}
-			}
+			player.ProcessEvent(msg);
 		}
 
-		if (is_move_up) player_pos.y -= PLAYER_SPEED;
-		if (is_move_down) player_pos.y += PLAYER_SPEED;
-		if (is_move_left) player_pos.x -= PLAYER_SPEED;
-		if (is_move_right) player_pos.x+= PLAYER_SPEED;
-
-		static int counter = 0;
-		if (++counter % 5 == 0)
-		{
-			idx_current_anim++;
-		}
-
-		idx_current_anim = idx_current_anim % PLAYER_ANIM_NUM;
+		player.Move();
+		TryGenerateEnemy(enemy_list);
+		for (Enemy* enemy : enemy_list)
+			enemy->Move(player);
 
 		cleardevice();
 
 		putimage(0, 0, &img_background);
-		putimage_alpha(player_pos.x, player_pos.y, &img_player_left[idx_current_anim]);
+		player.Draw(1000 / 144);
+		for (Enemy* enemy : enemy_list)
+			enemy->Draw(1000 / 144);
 
 		FlushBatchDraw();
 
